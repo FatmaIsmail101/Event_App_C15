a4a51a1e3d03b7a6b413c7e2c611e4d637784060
FatmaIsmail101 <ismailfatma197@gmail.com>
2025-07-15 23:02:23 +0300

    Create OnBoarding Folder Create OnBoardingScreen

--- lib/core/theme/color_pallete.dart
+++ lib/core/theme/color_pallete.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class ColorPallete{
   static const Color primaryColor=Color(0xff5669FF);
+  static const Color black=Color(0xff1C1C1C);
+  static const Color grey=Color(0xff7B7B7B);
+
 }
