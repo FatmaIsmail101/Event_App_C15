4883db7c5e6c80bcec9b75f782172225826108c9
FatmaIsmail101 <ismailfatma197@gmail.com>
2025-07-15 23:57:34 +0300

    Login Screen

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,11 +1,5 @@
 abstract class AppAssets{
   static const String appLogo='assets/icons/Logo.png';
   static const String copyRightsLogo='assets/icons/copy rights.png';
-  static const String appLogoBoarding='assets/icons/onboarding Icon.png';
-  static const String onBoardingOne='assets/icons/hot-trending.png';
-  static const String onBoardingTwo='assets/icons/being-creative.png';
-  static const String onBoardingThree='assets/icons/social-media.png';
-
-
 
 }
