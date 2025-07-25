e05eef9235f586f09d449ac14c4dd32d1045f1fc
FatmaIsmail101 <ismailfatma197@gmail.com>
2025-07-25 23:57:17 +0300

    login screen

--- lib/core/widgets/custom_button_style.dart
+++ lib/core/widgets/custom_button_style.dart
@@ -2,7 +2,6 @@ import 'package:evently/core/theme/color_pallete.dart';
 import 'package:flutter/material.dart';
 
 class CustomButtonStyle extends StatelessWidget {
-   const CustomButtonStyle({super.key
    ,this.color,
    required this.child,
      this.onTap
